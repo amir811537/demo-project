d4b98033d674a24b24ec8e3447a3ffd675aa5275
SAJIB6260 <s.k.sajib6260@gmail.com>
2024-01-16 15:55:52 +0600

    create a form

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <>
      <h1 className="text-3xl bg-green-400"> hello world !!</h1>
+     <form action="">
+      <input type="text" />
+      <input type="email" name="" id="" />
+      <input type="password" name="" id="" />
+     </form>
     </>
   )
 }
